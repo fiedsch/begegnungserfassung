4eb6bd29d738e6dc768d9d251b3cbc610e91f2c2
Andreas Fieger <fiedsch@ja-eh.at>
2019-03-22 08:45:16 +0100

    Update build script for new test environment

--- build.sh
+++ build.sh
@@ -9,6 +9,17 @@
 #      "lib": "vue-cli-service build --target lib --name begegnungserfassung src/main.js"
 #    }
 
+# Zielverzeichnis für die Produktion (Eingabe im BE)
+#TARGET_DIR=/Users/andreas/Project/edart-bayern.de/ligaverwaltung-bundle/src/Resources/public
+
+# Zielverzeichnis für die Tests == Eingabe im FE
+TARGET_DIR=/Users/andreas/Sites/edart-bayern.de-c4/files/assets/test  
+
 npm run lib && \
-  sudo cp dist/begegnungserfassung.css* /Users/andreas/Project/edart-bayern.de/ligaverwaltung-bundle/src/Resources/public
-  sudo cp dist/begegnungserfassung.umd.* /Users/andreas/Project/edart-bayern.de/ligaverwaltung-bundle/src/Resources/public
+  sudo cp dist/begegnungserfassung.css* ${TARGET_DIR}
+  sudo cp dist/begegnungserfassung.umd.* ${TARGET_DIR}
+
+cd /Users/andreas/Sites/edart-bayern.de-c4/
+./manage.sh
+./vendor/bin/contao-console contao:filesync
+cd -  
