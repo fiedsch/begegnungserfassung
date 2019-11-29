cdefc13b2a5cd2877ac485702a1abb33f18f752c
Andreas Fieger <fiedsch@ja-eh.at>
2019-11-29 14:29:42 +0100

    refactor: build as library
    * src/App.vue
    * src/main.js
    * public/index.html
    are only used for development when running npm run serve

---
+++ src/components/BegegnungsErfassung.vue
@@ -0,0 +1,130 @@
+<template ref="app">
+    <div>
+        <div v-if="isBackendView" id="tl_buttons">
+            <a href="contao?do=liga.begegnungserfassung" class="header_back" title="" accesskey="b" onclick="Backend.getScrollOffset()">Zurück</a>
+        </div>
+        <Aufstellung :showspielerpass="showspielerpass"></Aufstellung>
+        <div class="showspielerpasscheckbox">
+            <input type="checkbox" id="showspielerpass" v-model="showspielerpass">
+            <label for="showspielerpass">Spielerpass-Nummern anzeigen</label>
+        </div>
+        <form method="POST" id="vue_begegnungserfassung" class="tl_form tl_edit_form" enctype="application/x-www-form-urlencoded"><!-- multipart/form-data -->
+            <div class="tl_formbody_edit">
+                <input type="hidden" name="REQUEST_TOKEN" :value="requestToken">
+                <input type="hidden" name="FORM_SUBMIT" value="begegnungserfassung">
+                <!-- Auch json_data sollten wir nicht brauchen, ABER: siehe dataToSubmit() -->
+                <input type="hidden" name="json_data" :value="dataToSubmit">
+            </div>
+        </form>
+        <ResultsTable :showspielerpass="showspielerpass"></ResultsTable>
+        <HighlightsEntry :showspielerpass="showspielerpass"></HighlightsEntry>
+        <div class="tl_formbody_submit">
+            <div class="tl_submit_container">
+                <button type="submit" name="save" id="save" class="btn btn-primary tl_submit" accesskey="s" @click.prevent="saveFormData">Speichern</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Aufstellung from './Aufstellung.vue'
+import ResultsTable from './ResultsTable.vue'
+import HighlightsEntry from './HighlightsEntry'
+
+export default {
+    name: 'begegnungs-erfassung',
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    },
+    components: {
+        Aufstellung,
+        ResultsTable,
+        HighlightsEntry
+    },
+    data() {
+        return {
+            showspielerpass: false
+        }
+    },
+    computed: {
+        isBackendView() {
+            return this.$store.state.isBackendView
+        },
+        // Für das <form>
+        requestToken() {
+            return this.$store.state.requestToken
+        },
+        begegnungId() {
+            return this.$store.state.begegnungId
+        },
+        dataToSubmit() {
+            return JSON.stringify({
+                spielplan: this.$store.state.spielplan,
+                spielplan_code: this.$store.state.spielplan_code,
+                home: this.$store.state.home,
+                away: this.$store.state.away,
+                highlights: this.$store.state.highlights,
+                begegnungId: this.$store.state.begegnungId,
+                REQUEST_TOKEN: this.$store.state.requestToken,
+                FORM_SUBMIT: 'begegnungserfassung'
+            })
+        }
+    },
+    methods: {
+        saveFormData() {
+            // URL aus der Contao-Installation mit installiertem ligaverwaltung-bundle
+            let formData = new FormData(document.querySelector('#vue_begegnungserfassung'));
+            // vs.
+            // let formData = this.dataToSubmit
+            // ... was dann (aus Contaos Sicht?) aber ohne REQUEST_TOKEN ist.
+            // Auch wenn wir dieses in dataToSubmit einbauen "mault" Contao!
+
+            console.log(formData.get('json_data')); alert('FAKE: habe Daten gepostet (siehe console)'); return;
+
+            let url = '/ligaverwaltung/erfassen/'+this.begegnungId;
+            this.$http.post(url, formData).then(
+                (result) => {
+                    alert(JSON.stringify(result.data, ' ', 2))
+                    // TODO: anstelle des alert()s ein visuelles Feedback für
+                    // den User, daß das Speichern seiner Daten erfolgreich war.
+                }
+            ).catch(
+                (error) => {
+                    alert(error)
+                }
+            )
+        },
+        setData(data) {
+            this.$store.dispatch("setNumSlots", data.numSlots)
+            this.$store.dispatch("setSpielplan", data.spielplan)
+            this.$store.dispatch("setHome", data.home)
+            this.$store.dispatch("setAway", data.away)
+            this.$store.dispatch("setRequestToken", undefined !== data.requestToken ? data.requestToken : '')
+            this.$store.dispatch("setBegegnungId", undefined !== data.begegnungId ? data.begegnungId : 0)
+            this.$store.dispatch("initializeData")
+            this.$store.dispatch("setSpielplanCode", data.spielplan_code)
+            this.$store.dispatch("setIsBackendView", undefined !== data.isBackendView ? data.isBackendView : false)
+            this.$store.dispatch("setHighlightsData", undefined !== data.highlights ? data.highlights : {})
+        }
+    },
+    mounted() {
+        this.setData(this.data)
+    }
+}
+</script>
+
+<style>
+.tl_formbody_edit {
+    padding: 1rem;
+}
+.showspielerpasscheckbox {
+    margin-top: 1em;
+}
+.showspielerpasscheckbox label {
+    margin-left: .5em;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,21 +3,12 @@ import App from './App.vue'
 import axios from 'axios'
 import store from './store'
 
+
 Vue.config.productionTip = false
+
 Vue.prototype.$http = axios
 
-// Set data from globally defined variable
-// eslint-disable-next-line
-var appdata = data !== undefined ? data : { }
-
-var vm = new Vue({
+new Vue({
     store,
-    el: '#begegnungserfassung',
-    render: h => h(App)
-})
-
-/*
- * FIXME(?): Es sollte eleganter gehen, als davon auszugehen, daß wir genau ein
- * Kind-Element haben (die App-Komponente) und dann dieses zu verwenden.
- */
-vm.$children[0].setData(appdata)
+    render: h => h(App),
+}).$mount('#app')
