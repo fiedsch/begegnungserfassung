9b262e8587609986600b7bacdefcf5503cbcb6bd
Andreas Fieger <fiedsch@ja-eh.at>
2019-12-01 07:24:34 +0100

    remove axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 import store from './store'
 
-
 Vue.config.productionTip = false
-
-Vue.prototype.$http = axios
 
 new Vue({
     store,
