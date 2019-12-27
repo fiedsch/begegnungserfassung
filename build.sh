817c0f6e20d83a8009fc2f362baa6225a987204d
Andreas Fieger <fiedsch@ja-eh.at>
2019-12-27 07:43:50 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
-# In package.json folgenden Eintrag zu scripts hinzufügen, um npm run lib
-# ausführen zu können:
-#   "scripts": {
-#      "serve": "vue-cli-service serve",
-#      "build": "vue-cli-service build",
-#      "lint": "vue-cli-service lint",
-#      "lib": "vue-cli-service build --target lib --name begegnungserfassung src/main.js"
-#    }
+# Zielverzeichnis (Entwicklungsumgebung Contao Bundle)
+TARGET_DIR=/Users/andreas/Sites/edart-bayern.de/local-dev-bundles/ligaverwaltung-bundle/src/Resources/public
 
-# Zielverzeichnis für die Produktion (Eingabe im BE)
-#TARGET_DIR=/Users/andreas/Project/edart-bayern.de/ligaverwaltung-bundle/src/Resources/public
+for type in js css
+do 
+  if [ ! -d ${TARGET_DIR}/${type} ]
+  then
+    echo "erzeuge Verzeichnis ${TARGET_DIR}/${type}"
+    mkdir ${TARGET_DIR}/${type}
+  fi
+done
 
-# Zielverzeichnis für die Tests == Eingabe im FE
-TARGET_DIR=/Users/andreas/Sites/edart-bayern.de-c4/files/assets/test  
+npm run build && \
+  sudo cp dist/css/app.css ${TARGET_DIR}/css && \
+  sudo cp dist/js/app.js ${TARGET_DIR}/js  && \
+  sudo cp dist/js/chunk-vendors.js ${TARGET_DIR}/js
 
-npm run lib && \
-  sudo cp dist/begegnungserfassung.css* ${TARGET_DIR}
-  sudo cp dist/begegnungserfassung.umd.* ${TARGET_DIR}
-
-cd /Users/andreas/Sites/edart-bayern.de-c4/
+cd /Users/andreas/Sites/edart-bayern.de/
 ./manage.sh
-./vendor/bin/contao-console contao:filesync
-cd -  
+cd -
