30a6bc8be26c182cebafd048499df8e25423c9ea
Andreas Fieger <fiedsch@ja-eh.at>
2023-01-27 09:37:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Zielverzeichnis (Entwicklungsumgebung Contao Bundle)
-TARGET_DIR=/Users/andreas/Sites/edart-bayern.de/local-dev-bundles/ligaverwaltung-bundle/src/Resources/public
+TARGET_DIR=/Users/andreas/Project/edart-bayern.de/contao-ligaverwaltung-bundle/src/Resources/public
 
 for type in js css
 do 
@@ -13,9 +13,9 @@ do
 done
 
 npm run build && \
-  sudo cp dist/css/app.css ${TARGET_DIR}/css && \
-  sudo cp dist/js/app.js ${TARGET_DIR}/js  && \
-  sudo cp dist/js/chunk-vendors.js ${TARGET_DIR}/js
+  cp dist/css/app.css ${TARGET_DIR}/css && \
+  cp dist/js/app.js ${TARGET_DIR}/js  && \
+  cp dist/js/chunk-vendors.js ${TARGET_DIR}/js
 
 cd /Users/andreas/Sites/edart-bayern.de/
 ./manage.sh
